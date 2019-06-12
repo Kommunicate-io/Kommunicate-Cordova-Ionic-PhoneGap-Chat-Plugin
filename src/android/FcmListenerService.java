b5d06d1d5327d40454a7153e74e2447f5f31daf5
Ashish <reytum@live.com>
2019-06-12 23:43:24 +0530

    update plugin with latest fcm changes

--- src/android/FcmListenerService.java
+++ src/android/FcmListenerService.java
@@ -3,10 +3,12 @@ package io.kommunicate.phonegap.notification;
 
 import android.util.Log;
 
+import com.applozic.mobicomkit.Applozic;
+import com.applozic.mobicomkit.api.account.register.RegisterUserClientService;
 import com.applozic.mobicomkit.api.account.register.RegistrationResponse;
 import com.applozic.mobicomkit.api.account.user.MobiComUserPreference;
-import com.applozic.mobicomkit.api.account.user.PushNotificationTask;
 import com.applozic.mobicomkit.api.notification.MobiComPushReceiver;
+import com.applozic.mobicommons.commons.core.utils.Utils;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -27,7 +29,20 @@ public class FcmListenerService extends FirebaseMessagingService {
                 return;
             }
         }
-
     }
 
+    @Override
+    public void onNewToken(String registrationId) {
+        super.onNewToken(registrationId);
+
+        Utils.printLog(this, TAG, "Found Registration Id:" + registrationId);
+        Applozic.getInstance(this).setDeviceRegistrationId(registrationId);
+        if (MobiComUserPreference.getInstance(this).isRegistered()) {
+            try {
+                RegistrationResponse registrationResponse = new RegisterUserClientService(this).updatePushNotificationId(registrationId);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
